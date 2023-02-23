c5eb1703d3af189b34d98c11aaefab8c5321478a
José Maria Mendes <109546099+jmariamendes@users.noreply.github.com>
2023-02-23 15:24:43 -0300

    Remover a secret key

--- tef/settings.py
+++ tef/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$j8oc4ru^@*a*651w5qle=un=&mdwbxwcvujiu58#9*q%@&zp+'
+# SECRET_KEY = 'django-insecure-$j8oc4ru^@*a*651w5qle=un=&mdwbxwcvujiu58#9*q%@&zp+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
