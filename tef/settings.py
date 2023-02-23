30d6c55583096d777ef86d3b43522d2fd6b59821
José Maria Mendes <109546099+jmariamendes@users.noreply.github.com>
2023-02-23 17:41:59 -0300

    correção staticfile

--- tef/settings.py
+++ tef/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,7 +119,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
